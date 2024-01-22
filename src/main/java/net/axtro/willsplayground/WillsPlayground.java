cb9c0f223d2b66b4ccc8b45fd6f2028b321411c3
NotAxtro <77877788+NotAxtro@users.noreply.github.com>
2024-01-22 19:15:36 +0100

    Things added: English Language (US) Will's Beer (not able to use, yet) Creative Tab
    Overall just fixed some things and updated some files

--- src/main/java/net/axtro/willsplayground/WillsPlayground.java
+++ src/main/java/net/axtro/willsplayground/WillsPlayground.java
@@ -1,6 +1,11 @@
 package net.axtro.willsplayground;
 
 import com.mojang.logging.LogUtils;
+import net.axtro.willsplayground.block.ModBlocks;
+import net.axtro.willsplayground.item.ModCreativeTabs;
+import net.axtro.willsplayground.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -18,28 +23,30 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(WillsPlayground.MOD_ID)
 public class WillsPlayground {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "willsplayground";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public WillsPlayground() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
-
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(ModItems.BEER.get());
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/axtro/willsplayground/item/ModCreativeTabs.java
@@ -0,0 +1,33 @@
+package net.axtro.willsplayground.item;
+
+import net.axtro.willsplayground.WillsPlayground;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, WillsPlayground.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> PLAYGROUND_TAB = CREATIVE_MODE_TABS.register("playground_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.BEER.get()))
+                    .title(Component.translatable("creativetab.playground_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+
+
+                        pOutput.accept(ModItems.BEER.get());
+
+
+
+                    })
+                    .build());
+
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
